1f5d97181759ab29bb8838a081d9e8a614adaa5d
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-07 18:16:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end #this work
+
+def batch_badge_creator(name)
+name.collect {|name|  "Hello, my name is #{name}."}
+end #this work
+
+def assign_rooms(attendees)
+attendees.each_with_index.collect {|name, index|  "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+end
+
+def printer
+end
